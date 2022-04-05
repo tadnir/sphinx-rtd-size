ca72839ea80dfd55cea819086ecff7b9ae665206
michael tadnir <tadnir50@gmail.com>
2022-04-06 01:14:42 +0300

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ copyright = '2022, michael tadnir'
 author = 'michael tadnir'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
